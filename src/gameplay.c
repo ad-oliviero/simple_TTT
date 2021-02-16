ad4c81a3fadfae906647a9fce952034bae9baf59
Adriano Oliviero <adri@localhost.localdomain>
2021-02-16 15:33:21 +0100

    First commit, project already existing

---
+++ src/gameplay.c
@@ -0,0 +1,116 @@
+#ifdef __linux__
+	#include <raylib.h>
+#elif _WIN32
+	#include "include/raylib.h"
+#endif
+#include <unistd.h>
+#include "include/main.h"
+
+extern int game_grid[9];
+extern int is_game_over;
+extern bool turn;
+extern int winsP1;
+extern int winsP2;
+
+int checkwinner() {
+	if ((game_grid[0] == game_grid[1]) && (game_grid[1] == game_grid[2])) {
+		if (game_grid[0] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[0] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[3] == game_grid[4]) && (game_grid[4] == game_grid[5])) {
+		if (game_grid[3] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[3] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[6] == game_grid[7]) && (game_grid[7] == game_grid[8])) {
+		if (game_grid[6] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[6] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[0] == game_grid[3]) && (game_grid[3] == game_grid[6])) {
+		if (game_grid[0] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[0] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[1] == game_grid[4]) && (game_grid[4] == game_grid[7])) {
+		if (game_grid[1] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[1] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[2] == game_grid[5]) && (game_grid[5] == game_grid[8])) {
+		if (game_grid[2] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[2] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[0] == game_grid[4]) && (game_grid[4] == game_grid[8])) {
+		if (game_grid[0] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[0] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if ((game_grid[2] == game_grid[4]) && (game_grid[4] == game_grid[6])) {
+		if (game_grid[2] == 1) {
+			is_game_over = 1;
+			return 1;
+		} else if (game_grid[2] == 2) {
+			is_game_over = 1;
+			return 2;
+		}
+	} else if (game_grid[0] != 0 && game_grid[1] != 0 && game_grid[2] != 0 && game_grid[3] != 0 && game_grid[4] != 0 && game_grid[5] != 0 && game_grid[6] != 0 && game_grid[7] != 0 && game_grid[8] != 0) {
+		is_game_over = 1;
+		return 3;
+	}
+	return 0;
+}
+
+void endGame(int winner) {
+	DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(RAYWHITE, .5f));
+	Rectangle restart_btn = {SCR_WIDTH / 5, SCR_HEIGHT / 3, SCR_WIDTH / 5 * 3, SCR_HEIGHT / 10};
+	DrawRectangleRounded(restart_btn, .7f, 20, DARKGRAY);
+	DrawRectangleRoundedLines(restart_btn, .7f, 100, 5, RED);
+
+	if (winner == 1) {
+		DrawText("Player 1 (X) WON!", SCR_WIDTH / 3.3f, SCR_HEIGHT / 2.7f, 20, BLACK);
+	} else if (winner == 2) {
+		DrawText("Player 2 (O) WON!", SCR_WIDTH / 3.3f, SCR_HEIGHT / 2.7f, 20, BLACK);
+	} else if (winner == 3) {
+		DrawText("Draw...", SCR_WIDTH / 2.45f, SCR_HEIGHT / 2.74f, 30, BLACK);
+	}
+
+	if (CheckCollisionPointRec(GetMousePosition(), restart_btn) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+		for (int i = 0; i < 9; i ++) {
+			game_grid[i] = 0;
+		}
+		if (winner == 1) {
+			winsP1++;
+			turn = !turn;
+		} else if (winner == 2) {
+			winsP2++;
+			turn = !turn;
+		}
+		is_game_over = 0;
+		turn = !turn;
+		usleep(150000);
+	}
+}
---
+++ src/main.c
@@ -0,0 +1,51 @@
+#ifdef __linux__
+	#include <raylib.h>
+#elif _WIN32
+	#include "include/raylib.h"
+#endif
+#include <stdio.h>
+#include "include/gameplay.h"
+#include "include/gui.h"
+#include "include/main.h"
+#include "include/shapes.h"
+
+int block = SCR_WIDTH / 3;
+Rectangle game[9];
+int game_grid[9] = {
+	0, 0, 0,
+	0, 0, 0,
+	0, 0, 0
+};
+int is_game_over = 0;
+bool turn = true;
+int winsP1 = 0;
+int winsP2 = 0;
+
+int main() {
+	InitWindow(SCR_WIDTH, SCR_HEIGHT, PROGRAM_NAME);
+	SetTargetFPS(GetMonitorRefreshRate(0));
+	
+	initHitBox();
+	// --- MAIN GAME LOOP ---
+	while (!WindowShouldClose()) {
+		checkwinner();
+		if (turn) {
+			cross(game);
+		} else {
+			circle(game);
+		}
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		grid();
+		for (int i = 0; i < 9; i++) {
+			shape(game, &i, &game_grid[i]);
+		}
+		if (is_game_over) {
+			endGame(checkwinner());
+		}
+		matchInfo();
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
+}
